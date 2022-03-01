29659cfac04f1af1166de0269b955a2cb4770728
Go Kudo <g-kudo@colopl.co.jp>
2022-03-01 15:00:02 +0900

    drop support: XorShift128Plus, PCG64

--- ext/random/random.stub.php
+++ ext/random/random.stub.php
@@ -57,49 +57,11 @@ namespace Random\Engine
         public function __debugInfo(): array {}
     }
 
-    class PCG64 implements Random\SeedableEngine, Random\SerializableEngine
-    {
-        public function __construct(string|int|null $seed = null) {}
-
-        /** @implementation-alias Random\Engine\CombinedLCG::generate */
-        public function generate(): string {}
-
-        public function jump(int $advance): void {}
-        
-        /** @implementation-alias Random\Engine\CombinedLCG::__serialize */
-        public function __serialize(): array {}
-
-        /** @implementation-alias Random\Engine\CombinedLCG::__unserialize */
-        public function __unserialize(array $data): void {}
-
-        /** @implementation-alias Random\Engine\CombinedLCG::__debugInfo */
-        public function __debugInfo(): array {}
-    }
-
     /** @not-serializable */
     class Secure implements Random\CryptoSafeEngine
     {
         /** @implementation-alias Random\Engine\CombinedLCG::generate */
         public function generate(): string {}
-    }
-
-    class XorShift128Plus implements Random\SeedableEngine, Random\SerializableEngine
-    {
-        public function __construct(string|int|null $seed = null) {}
-
-        /** @implementation-alias Random\Engine\CombinedLCG::generate */
-        public function generate(): string {}
-
-        public function jump(): void {}
-
-        /** @implementation-alias Random\Engine\CombinedLCG::__serialize */
-        public function __serialize(): array {}
-
-        /** @implementation-alias Random\Engine\CombinedLCG::__unserialize */
-        public function __unserialize(array $data): void {}
-
-        /** @implementation-alias Random\Engine\CombinedLCG::__debugInfo */
-        public function __debugInfo(): array {}
     }
 
     class Xoshiro256StarStar implements Random\SeedableEngine, Random\SerializableEngine
--- ext/random/tests/02_engine/user_compatibility.phpt
+++ ext/random/tests/02_engine/user_compatibility.phpt
@@ -21,24 +21,6 @@ for ($i = 0; $i < 1000; $i++) {
     }
 }
 
-$native_engine = new \Random\Engine\PCG64(1234);
-$user_engine = new class (1234) extends \Random\Engine\PCG64 {};
-
-for ($i = 0; $i < 1000; $i++) {
-    if ($native_engine->generate() !== $user_engine->generate()) {
-        die('failure PCG64');
-    }
-}
-
-$native_engine = new \Random\Engine\XorShift128Plus(1234);
-$user_engine = new class (1234) extends \Random\Engine\XorShift128Plus {};
-
-for ($i = 0; $i < 1000; $i++) {
-    if ($native_engine->generate() !== $user_engine->generate()) {
-        die('failure XorShift128Plus');
-    }
-}
-
 $native_engine = new \Random\Engine\Xoshiro256StarStar(1234);
 $user_engine = new class (1234) extends \Random\Engine\Xoshiro256StarStar {};
 
