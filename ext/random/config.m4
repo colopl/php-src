098b27de358d1510fa02391457a8c0af3b45028a
Go Kudo <g-kudo@colopl.co.jp>
2022-07-07 17:40:44 +0900

    split user

--- ext/random/config.m4
+++ ext/random/config.m4
@@ -6,5 +6,5 @@ AC_CHECK_DECLS([arc4random_buf])
 dnl
 dnl Setup extension
 dnl
-PHP_NEW_EXTENSION(random, random.c random_combinedlcg.c random_mersennetwister.c random_pcg64s.c random_secure.c , , , -DZEND_ENABLE_STATIC_TSRMLS_CACHE=1)
+PHP_NEW_EXTENSION(random, random.c random_combinedlcg.c random_mersennetwister.c random_pcg64s.c random_secure.c random_user.c , , , -DZEND_ENABLE_STATIC_TSRMLS_CACHE=1)
 PHP_INSTALL_HEADERS([ext/random])
--- ext/random/random.c
+++ ext/random/random.c
@@ -291,57 +291,6 @@ static zend_object *php_random_engine_secure_new(zend_class_entry *ce)
 	return &php_random_engine_common_init(ce, &random_engine_secure_object_handlers, &php_random_algo_secure)->std;
 }
 
-/* ---- USER BEGIN ---- */
-static uint64_t user_generate(php_random_status *status)
-{
-	php_random_status_state_user *s = status->state;
-	uint64_t result = 0;
-	size_t size;
-	zval retval;
-	uint32_t i;
-
-	zend_call_known_instance_method_with_0_params(s->generate_method, s->object, &retval);
-
-	/* Store generated size in a state */
-	size = Z_STRLEN(retval);
-
-	/* Guard for over 64-bit results */
-	if (size > sizeof(uint64_t)) {
-		size = sizeof(uint64_t);
-	}
-	status->last_generated_size = size;
-
-	if (size > 0) {
-		/* Endianness safe copy */
-		for (i = 0; i < size; i++) {
-			result += ((uint64_t) (unsigned char) Z_STRVAL(retval)[i]) << (8 * i);
-		}
-	} else {
-		status->last_unsafe = true;
-		return 0;
-	}
-
-	zval_ptr_dtor(&retval);
-
-	return result;
-}
-
-static zend_long user_range(php_random_status *status, zend_long min, zend_long max)
-{
-	return php_random_range(&php_random_algo_user, status, min, max);
-}
-
-const php_random_algo php_random_algo_user = {
-	0,
-	sizeof(php_random_status_state_user),
-	NULL,
-	user_generate,
-	user_range,
-	NULL,
-	NULL,
-};
-/* ---- USER END ---- */
-
 /* ---- Randomizer BEGIN ---- */
 static zend_object *php_random_randomizer_new(zend_class_entry *ce)
 {
